aa2bd84fc7f54de956557b94c2b48dd4f20c79db
matan lehrer <mathin297@gmail.com>
2024-07-21 17:57:40 +0300

    Initial layout of project
    I created 2 folders - frontend and backend.
    I installed all the necessary packages - npm, node.js, react, postgress, fastapi
    the files in the folders are templates used for making sure the packages work.

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from backend import crud, models, schemas
+from backend.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
